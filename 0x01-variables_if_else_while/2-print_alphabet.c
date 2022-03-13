cda6b65d57ba733789d5c00389b50afcaf389451
hozas93 <nkwentihozagiresse@gmail.com>
2022-03-13 03:18:18 -0700

    exercises

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -18,33 +18,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
-3-
-#include <stdio.h>
-/**
- * main - Program entry point
- *
- * Return: 0 on success. Error code otherwise
- */
-int main(void)
-{
-	char a = 'a';
-	char A = 'A';
-	int n = 0;
-
-	while (n < 52)
-	{
-		if (n < 26)
-		{
-			putchar(a);
-			a++;
-		} else
-		{
-			putchar(A);
-			A++;
-		}
-		n++;
-	}
-	putchar('\n');
-	return (0);
-}
