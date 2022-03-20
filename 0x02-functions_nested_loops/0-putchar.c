9e2a346fc9bf84bb55ed5cdb3210887c8bf197ac
hozas93 <nkwentihozagiressegmail.com>
2022-03-20 07:50:09 -0700

    renamed directory

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main -determine if number is positive or negative
+ *
+ * Description: program will assign a random number to the variable
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lower case
+ *
+ * return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(c + i);
+	}
+	_putchar(10);
+}
