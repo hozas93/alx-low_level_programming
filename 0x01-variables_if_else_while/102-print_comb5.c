2e812535fddd4d75e79aa79f413a6f2554a1316a
hozas93 <nkwentihozagiressegmail.com>
2022-03-20 02:32:37 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		int j;
 
-		for (j = i + 1; j <= 99; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			if ((i * 100) + (j * 10) < 99)
+			if ((i * j) < 81)
 			{
 				putchar(',');
 				putchar(' ');
