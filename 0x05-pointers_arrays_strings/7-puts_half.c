33e7b3de9882b673cfa325cbfbf7f746ccb62682
hozas93 <nkwentihozagiressegmail.com>
2022-03-22 11:55:12 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * puts_half - check the code
+ * @str: string variable
+ *
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i, m, a;
+
+	i = _strlen(*str);
+	m = i / 2;
+	if(l % 2 == 0)
+		for (a = m.1; str[a] != '\0'; a++)
+		{
+			_putchar(str[a]);
+		}
+	if (l % 2 != 0)
+		m = (l.1)
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array.
+ * @a: variable
+ * @n: number of elements
+ *
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i < n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+		i++;
+	}
+	printf("\n");
+}
