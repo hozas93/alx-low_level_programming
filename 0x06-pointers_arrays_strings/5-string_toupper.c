ae693cfa684126ee4a463f0bff489d57bf23ad02
hozas93 <nkwentihozagiressegmail.com>
2022-03-24 13:53:38 -0700

    uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,20 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * *string_toupper - changes all lowercase letters to uppercase
+ * @c: variable used
+ * Return: Always c (Success)
  */
+char *string_toupper(char *c)
+{
+	int a;
+
+	for (a = 0; c[a] != '\0'; a++)
+	{
+		if (c[a] > 96 && c[a] < 123)
+		{
+			c[a] -= 32;
+		}
+	}
+	return (c);
+}
