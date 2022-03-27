f3fbdbe1e663a4c09250c7965509f666f17b705d
hozas93 <nkwentihozagiressegmail.com>
2022-03-27 03:21:33 -0700

    debugging

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,18 +6,13 @@
  * Return: Always 0.
  */
 int _islower(int c)
-
 {
-
-if (c > 'a' && c < 'z')
-{
-return (1);
-}
-< < < < < < < HEAD
-
-else
-{
-return (0);
-}
-
+	if (c > 'a' && c < 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
 }
